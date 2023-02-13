1877e6ccb608ce10f363db5db8c4a9eb6fe31831
Artem <shvachev91@gmail.com>
2023-02-13 15:49:53 +0200

    tests

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,5 +1,6 @@
 import { profileApiPage } from '../api/api.js'
 const ADD_POST = 'ADD-POST'
+const DELETE_POST = 'DELETE_POST'
 const SET_USER_PROFILE = 'SET_USER_PROFILE'
 const SET_STATUS = 'SET-STATUS'
 
@@ -42,6 +43,12 @@ const profileReducer = (state = initialState, action) => {
         ],
       }
     }
+    case DELETE_POST: {
+      return {
+        ...state,
+        posts: state.posts.filter((p) => p.id != action.postId),
+      }
+    }
 
     case SET_USER_PROFILE: {
       return { ...state, profile: action.profile }
@@ -58,6 +65,11 @@ const profileReducer = (state = initialState, action) => {
 export const addPostActionCreater = (formData) => ({
   type: ADD_POST,
   payload: formData,
+})
+
+export const deletePost = (postId) => ({
+  type: DELETE_POST,
+  postId,
 })
 
 export const setUserProfile = (profile) => ({
--- src/redux/profile-reducer.test.js
+++ src/redux/profile-reducer.test.js
@@ -1,35 +1,64 @@
-import profileReducer, { addPostActionCreater } from './profile-reducer'
+import profileReducer, {
+  addPostActionCreater,
+  deletePost,
+} from './profile-reducer'
+
+let state = {
+  posts: [
+    {
+      id: 1,
+      message: 'It is react',
+      likesCount: 12,
+    },
+    {
+      id: 2,
+      message: 'It is Js',
+      likesCount: 15,
+    },
+    {
+      id: 3,
+      message: 'It is Html',
+      likesCount: 10,
+    },
+    {
+      id: 4,
+      message: 'It is CSS',
+      likesCount: 20,
+    },
+  ],
+}
 
 it('length of posts should be incremented', () => {
   // 1. test data
   let action = addPostActionCreater('it-kamasutra')
-  let state = {
-    posts: [
-      {
-        id: 1,
-        message: 'It is react',
-        likesCount: 12,
-      },
-      {
-        id: 2,
-        message: 'It is Js',
-        likesCount: 15,
-      },
-      {
-        id: 3,
-        message: 'It is Html',
-        likesCount: 10,
-      },
-      {
-        id: 4,
-        message: 'It is CSS',
-        likesCount: 20,
-      },
-    ],
-  }
+
   // 2. action
   let newState = profileReducer(state, action)
 
   // 3. expection
   expect(newState.posts.length).toBe(5)
+})
+
+it('length of posts should be correct', () => {
+  // 1. test data
+  let action = addPostActionCreater('it-kamasutra')
+
+  // 2. action
+  let newState = profileReducer(state, action)
+
+  // 3. expection
+
+  expect(newState.posts[4].message).toBe('it-kamasutra')
+})
+
+it('after deleting length of message should be decrement', () => {
+  // 1. test data
+  let action = deletePost(1)
+
+  // 2. action
+  let newState = profileReducer(state, action)
+
+  // 3. expection
+
+  expect(newState.posts.length).toBe(3)
 })
