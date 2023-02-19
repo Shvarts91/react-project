4f6e5b081c11b1e8e35532a18ec9097a0d6ce267
Pavlo Tarasenko <pashkes@gmail.com>
2023-02-20 01:42:23 +0200

    Updates

--- precache-manifest.dd92174731beec2f589912ff7d99a406.js
+++ precache-manifest.dd92174731beec2f589912ff7d99a406.js
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "./static/media/preloader.6f09789b.gif"
   },
   {
-    "revision": "30167b40c54ec5070ca1911d420b58fd",
+    "revision": "a71244744f64ac176ef0995de8583c69",
     "url": "./index.html"
   }
 ];
