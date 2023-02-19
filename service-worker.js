7981858cd294fe355230d5876e47beed8c31d574
Pavlo Tarasenko <pashkes@gmail.com>
2023-02-20 01:35:20 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Shvarts91/react-project/precache-manifest.0dcaf2f2ce321d6808764a79ce879f3a.js"
+  "/precache-manifest.a163fe7d9e25de05c046a48ec1c831c8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Shvarts91/react-project/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
